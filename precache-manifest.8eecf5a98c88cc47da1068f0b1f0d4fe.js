078808c9cc1edd31b9785b8f84e5bcb418bcffab
defauth98 <neto.daniribeiro@gmail.com>
2021-07-21 16:23:10 -0300

    Updates

---
+++ precache-manifest.8eecf5a98c88cc47da1068f0b1f0d4fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff1979a7e51405e7b060e97e23113054",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "b9674d2f00478fc36b2a",
+    "url": "/frontend-online-store/static/css/main.ef7cfece.chunk.css"
+  },
+  {
+    "revision": "792972f5d005fdb488ba",
+    "url": "/frontend-online-store/static/js/2.9173843d.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.9173843d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9674d2f00478fc36b2a",
+    "url": "/frontend-online-store/static/js/main.17f5a84f.chunk.js"
+  },
+  {
+    "revision": "a2d408d9505863c5fc2c",
+    "url": "/frontend-online-store/static/js/runtime-main.2928fc3e.js"
+  }
+]);
