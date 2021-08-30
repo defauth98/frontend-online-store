45105376551806cdae4e60cf57cd5bf21c2dfa1c
defauth98 <neto.daniribeiro@gmail.com>
2021-08-30 12:06:20 -0300

    feat: estilizacao da tela de detalhe do produto

--- src/pages/ProductDetails/index.jsx
+++ src/pages/ProductDetails/index.jsx
@@ -2,56 +2,87 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-import Rating from '../../components/Rating';
+// import Rating from '../../components/Rating';
+
+import './styles.css';
 
 class ProductDetails extends React.Component {
   render() {
     const { location: { state: { title, price, thumbnail, attributes } } } = this.props;
-    const { addToCart, cartSize } = this.props;
+    const { addToCart } = this.props;
     const { match: { params: { itemId: id } } } = this.props;
 
     return (
-      <div>
-        <h1 data-testid="product-detail-name">{ title }</h1>
-        <h2>{` R$ ${price} `}</h2>
-        <img src={ thumbnail } alt={ title } />
-
-        <div>
-          <ul>
-            {attributes.map(({ value_name: value, name }, index) => (
-              <li key={ index }>
-                { `${name} : ${value}`}
-              </li>))}
-          </ul>
-        </div>
-
-        <button
-          type="button"
-          data-testid="product-detail-add-to-cart"
-          onClick={ () => addToCart({ title, price, thumbnail, id }) }
-        >
-          Adionar ao carrinho
-        </button>
-
-        <Link to="/Cart" data-testid="shopping-cart-button">
-          <button type="button">Cart</button>
-        </Link>
-
-        <p data-testid="shopping-cart-size">
-          { cartSize }
-        </p>
-
-        <div id="evaluate-container">
-          <input type="email" placeholder="mail@mail.com" />
-          <Rating />
-          <textarea
-            name="message"
-            id="input-message"
-            cols="30"
-            rows="10"
-            data-testid="product-detail-evaluation"
+      <div className="product-detail-page">
+        <div className="image-container">
+          <img
+            src={ thumbnail }
+            alt={ title }
+            className="product-image"
           />
-          <button id="button-evaluate" type="button">Avaliar produto</button>
+        </div>
+        <div className="infos-container">
+          <h1 data-testid="product-detail-name" className="product-title">{ title }</h1>
+          <h2 className="product-price">{` R$ ${price} `}</h2>
+          <div>
+            <ul className="product-info">
+              {attributes.map(({ value_name: value, name }, index) => (
+                <li key={ index } className="product-info-item">
+                  { `${name} : ${value}`}
+                </li>))}
+            </ul>
+          </div>
+
+          <button
+            type="button"
+            data-testid="product-detail-add-to-cart"
+            className="add-to-cart-btn btn"
+            onClick={ () => addToCart({ title, price, thumbnail, id }) }
+          >
+            Adicionar ao carrinho
+          </button>
+
+          <Link to="/Cart" data-testid="shopping-cart-button">
+            <button type="button" className="btn show-cart-btn">Ver carrinho</button>
+          </Link>
+
+          <div id="evaluate-container" className="evaluate-container">
+            <h3>Avaliar o produto</h3>
+
+            <label htmlFor="product-evaluation-email" className="evaluate-email">
+              Email
+              <br />
+              <input
+                type="email"
+                placeholder="mail@mail.com"
+                className="product-evaluation-email"
+                id="product-evaluation-email"
+              />
+            </label>
+
+            {/* <Rating /> */}
+
+            <label htmlFor="input-message" className="evaluate-text">
+              Avaliação
+              <textarea
+                name="message"
+                id="input-message"
+                cols="30"
+                rows="10"
+                data-testid="product-detail-evaluation"
+                className="product-evaluation-text"
+              />
+
+            </label>
+
+            <button
+              id="button-evaluate"
+              type="button"
+              className="product-evaluation-btn btn"
+            >
+              Avaliar produto
+            </button>
+          </div>
         </div>
       </div>
     );
